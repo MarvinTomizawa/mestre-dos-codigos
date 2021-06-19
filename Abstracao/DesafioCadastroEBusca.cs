695eb609553316a92ed863fcb1c58bb974f3ae80
MarvinTomizawa <marvin.tomizawa@db1.com.br>
2021-06-19 03:28:10 -0300

    refact: Abstraido comportamento de exercicio de cadastro de varias entidades para reaproveitar em outros exercicios

---
+++ Abstracao/DesafioCadastroEBusca.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abstracao
+{
+    public abstract class DesafioCadastroEBusca<T>: ExercicioBase<int>
+    {
+        protected DesafioCadastroEBusca(string mensagemParametro)
+        {
+            Parametros.Add(new ParametroExercicio<int>
+            {
+                Mensagem = mensagemParametro,
+                MensagemParametroInvalido = "O número informado deve ser inteiro"
+            });
+        }
+        
+        protected override Action<IList<ParametroExercicio<int>>> Action()
+            => parametros =>
+            {
+                var quantidadeEntidadesInformadas = parametros[0].Valor;
+                
+                if (quantidadeEntidadesInformadas < 1)
+                {
+                    Console.WriteLine("Informado número inválido para cadastro");
+                    Console.ReadLine();
+                    return;
+                }
+                
+                var entidades = PreencheEntidades(quantidadeEntidadesInformadas);
+                TrataEntidades(entidades);
+            };
+
+        protected abstract void TrataEntidades(IList<T> entidades);
+
+        private IList<T> PreencheEntidades(int quantidadeDeCadastros)
+        {
+            var entidades = new List<T>();
+            
+            for (var i = 0; i < quantidadeDeCadastros; i++)
+            {
+                entidades.Add(MontaEntidadeCadastro());
+
+                Console.Clear();
+            }
+
+            return entidades;
+        }
+
+        protected abstract  T MontaEntidadeCadastro();
+    }
+}
--- Exercicio2/Desafio2Base.cs
+++ Exercicio2/Desafio2Base.cs
@@ -1,64 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Abstracao;
 
 namespace Exercicio2
 {
-    public abstract class Desafio2Base: ExercicioBase<int>
+    public abstract class Desafio2Base: DesafioCadastroEBusca<Funcionario>
     {
-        public Desafio2Base()
+        protected  Desafio2Base() : base("Informe a quantidade de funcionarios para cadastrar")
         {
-            Parametros.Add(new ParametroExercicio<int>
+        }
+
+        protected override void TrataEntidades(IList<Funcionario> entidades)
+        {
+            var funcionarioBuscado = BuscaFuncionario(entidades);
+
+            MostraMensagem(funcionarioBuscado);
+        }
+
+        protected override Funcionario MontaEntidadeCadastro()
+        {
+            var nomeFuncionario = ValidadorInput.BuscaInput<string>("Informe o nome do funcionário",
+                "O nome deve ser um string");
+
+            var salarioFuncionario = ValidadorInput.BuscaInput<decimal>("Informe um salário para o funcionario",
+                "O salário deve ser double");
+
+            return new Funcionario
             {
-                Mensagem = "Informe o número de funcionários a ser cadastrados",
-                MensagemParametroInvalido = "O número informado deve ser inteiro"
-            });
+                Nome = nomeFuncionario,
+                Salario = salarioFuncionario
+            };
         }
         
-        protected override Action<IList<ParametroExercicio<int>>> Action()
-            => parametros =>
-            {
-                var quantidadeFuncionariosInformado = parametros[0].Valor;
-                
-                if (quantidadeFuncionariosInformado < 1)
-                {
-                    Console.WriteLine("Informado número inválido de funcionários");
-                    Console.ReadLine();
-                    return;
-                }
-                
-                var funcionarios = PreencheFuncionarios(quantidadeFuncionariosInformado);
-                var funcionarioBuscado = BuscaFuncionario(funcionarios);
-
-                MostraMensagem(funcionarioBuscado);
-            };
-
         protected abstract void MostraMensagem(Funcionario funcionario);
 
         protected abstract Funcionario BuscaFuncionario(IList<Funcionario> funcionarios);
 
-        private static IList<Funcionario> PreencheFuncionarios(int quantidadeFuncionarios)
-        {
-            var funcionarios = new List<Funcionario>();
-            
-            for (var i = 0; i < quantidadeFuncionarios; i++)
-            {
-                var nomeFuncionario = ValidadorInput.BuscaInput<string>("Informe o nome do funcionário",
-                    "O nome deve ser um string");
-
-                var salarioFuncionario = ValidadorInput.BuscaInput<decimal>("Informe um salário para o funcionario",
-                    "O salário deve ser double");
-
-                funcionarios.Add(new Funcionario
-                {
-                    Nome = nomeFuncionario,
-                    Salario = salarioFuncionario
-                });
-                
-                Console.Clear();
-            }
-
-            return funcionarios;
-        }
     }
 }

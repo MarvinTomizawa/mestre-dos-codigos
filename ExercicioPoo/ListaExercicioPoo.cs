06489d62811b48d93316c68c87cc97ac6b28292f
MarvinTomizawa <marvin.tomizawa@db1.com.br>
2021-06-21 01:07:57 -0300

    feat: Finalizado exercicio 2

---
+++ ExercicioPoo/ListaExercicioPoo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Abstracao;
+
+namespace ExercicioPoo
+{
+    public class ListaExercicioPoo : Exercicios
+    {
+        private readonly List<ExercicioBase<int>> _exercicios;
+
+        public ListaExercicioPoo()
+        {
+            _exercicios = new List<ExercicioBase<int>>
+            {
+                new Exercicio2()
+            };
+        }
+
+        public override int TamanhoDaLista()
+            => _exercicios.Count;
+
+        protected override void ExecutaExercicioNaLista(int opcao)
+            => _exercicios[opcao].ExecutaExercicio();
+
+        protected override string BuscaTituloNaLista(int opcao)
+            => _exercicios[opcao].GetTitulo();
+    }
+}
